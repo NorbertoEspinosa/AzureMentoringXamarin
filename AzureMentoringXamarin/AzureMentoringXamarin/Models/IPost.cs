fd239624d4afbf64c7ee6226a4fdd3525226e270
PLAINCONCEPTS\nespinosa <nespinosa@plainconcepts.com>
2018-06-05 09:30:03 +0200

    InitialBeforeTransformIntoBlog

---
+++ AzureMentoringXamarin/AzureMentoringXamarin/Models/IPost.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureMentoringXamarin.Models
+{
+    public class Ipost
+    {
+    }
+}
---
+++ AzureMentoringXamarin/AzureMentoringXamarin/Models/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureMentoringXamarin.Models
+{
+    public class Post : IPost
+    {
+        public Post(string title, string about, string content, string tags, int user, string createdAt)
+        {
+            Title = title;
+            About = about;
+            Content = content;
+            Tags = tags;
+            User = user;
+            CreatedAt = createdAt;
+        }
+
+        public Post()
+        { }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string About { get; set; }
+        public string Content { get; set; }
+        public string Tags { get; set; }
+        public int User { get; set; }
+        public string CreatedAt { get; set; }
+    }
+}
